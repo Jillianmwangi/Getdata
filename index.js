398648e21ae59a1208e8f0c5f7620ea0a5e0a8a7
Jillianmwangi <mwangyjill@gmail.com>
2023-01-31 08:53:59 +0300

    commit js file

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+//function using callbacks:
+
+const data = [ "Harry", "Ross", "Bruce",  "Cook", "Carolyn", "Morgan", "Albert", "Walker", "Randy", "Reed", "Larry", "Barnes", "Lois", "Wilson", "Jesse" , "Campbell", "Ernest" , "Rogers", "Theresa" , "Patterson", "Henry" , "Simmons", "Michelle" , "Perry", "Frank" , "Butler", "Shirley"];
+
+const getData = (callback) => {
+  setTimeout(() => {
+    callback(data);
+  }, 2000);
+};
+
+const displayData = (data) => {
+  console.log(data);
+};
+
+getData(displayData);
+
+
+
+
+//promise function
+
+
+const data = [ "Harry", "Ross", "Bruce",  "Cook", "Carolyn", "Morgan", "Albert", "Walker", "Randy", "Reed", "Larry", "Barnes", "Lois", "Wilson", "Jesse" , "Campbell", "Ernest" , "Rogers", "Theresa" , "Patterson", "Henry" , "Simmons", "Michelle" , "Perry", "Frank" , "Butler", "Shirley"];
+
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, 2000);
+  });
+};
+
+const displayData = (data) => {
+  console.log(data);
+};
+
+getData()
+  .then(displayData)
+  .catch((error) => {
+    console.error(error);
+  });
