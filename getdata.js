876f240d12784750094fe2eb4990b37fe1067e01
Jillianmwangi <mwangyjill@gmail.com>
2023-01-31 12:17:58 +0300

    add assignment files

---
+++ getdata.js
@@ -0,0 +1,20 @@
+//const data = [ "Harry", "Ross", "Bruce",  "Cook", "Carolyn", "Morgan", "Albert", "Walker", "Randy", "Reed", "Larry", "Barnes", "Lois", "Wilson", "Jesse" , "Campbell", "Ernest" , "Rogers", "Theresa" , "Patterson", "Henry" , "Simmons", "Michelle" , "Perry", "Frank" , "Butler", "Shirley"];
+
+const data = [];
+function getData (){
+        for(const names of data ){
+            console.log(names);
+        }
+}
+
+let promise = new Promise((resolve, reject)=>{
+    if(data != 0){
+        resolve(getData());
+    }else{
+        reject("The data set is empty");
+    }
+})
+.then(getData())
+.catch((error)=>{
+    console.log(error);
+})
--- index.js
+++ index.js
@@ -4,40 +4,11 @@
 
 const data = [ "Harry", "Ross", "Bruce",  "Cook", "Carolyn", "Morgan", "Albert", "Walker", "Randy", "Reed", "Larry", "Barnes", "Lois", "Wilson", "Jesse" , "Campbell", "Ernest" , "Rogers", "Theresa" , "Patterson", "Henry" , "Simmons", "Michelle" , "Perry", "Frank" , "Butler", "Shirley"];
 
-const getData = (callback) => {
-  setTimeout(() => {
-    callback(data);
-  }, 2000);
-};
+function getData(){
+  for(const names of data){
+    console.log(names);
+  }
+}
 
-const displayData = (data) => {
-  console.log(data);
-};
+setTimeout(getData, 2000);
 
-getData(displayData);
-
-
-
-
-//promise function
-
-
-const data = [ "Harry", "Ross", "Bruce",  "Cook", "Carolyn", "Morgan", "Albert", "Walker", "Randy", "Reed", "Larry", "Barnes", "Lois", "Wilson", "Jesse" , "Campbell", "Ernest" , "Rogers", "Theresa" , "Patterson", "Henry" , "Simmons", "Michelle" , "Perry", "Frank" , "Butler", "Shirley"];
-
-const getData = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(data);
-    }, 2000);
-  });
-};
-
-const displayData = (data) => {
-  console.log(data);
-};
-
-getData()
-  .then(displayData)
-  .catch((error) => {
-    console.error(error);
-  });
